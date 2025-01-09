49cee1e2a4e4b7ea5d89fb502795c343314440ef
Just Lmr <88000765+JustLmr@users.noreply.github.com>
2025-01-09 14:10:55 +0300

    secound

--- Saat/test/onlinealarmkur.com/assets/js/index.js
+++ Saat/test/onlinealarmkur.com/assets/js/index.js
@@ -6,9 +6,21 @@ const messages = [
   "Bu tam ekran saat, özellikle sınıfta öğrencilere zamanı göstermek veya evde egzersiz yapmak için size yardımcı olabilir. Bu saati kullanarak zamandan tasarruf edebilir ve her türlü işleriniz için zaman takibi yapmak adına faydalı olabilir.",
 ];
 var messages1 = [
-    "Yazı 1",
-    "Yazı 2",
-    "Yazı 3",
+    "Birinci: Genelde Pratisyen hekimler tarafından aile sağlığı merkzlerinde ayakta verilen tedavidir",
+    "Bulaşıcı: bağışıklama Bulaşıcı hastalıklardan korunmada çok önemli bir rol oynar aşılar vucüdün hastalıklara karşı dirncç kazanmasını sağlar ve bağışıklık sistemini güçlendirir bu sayede vücuda girmeden tanır ve yok edilir",
+    "Ergen: erkeklerde= Boy kilo artış. Kemiklerde İrişleşme",
+    "Ergen: kadınkarda= Boy kütle erkeklere göre az olur",
+    "Ergen: kadınkarda= kalçalar büyür kemikler genişler",
+
+    "Fiziksel aktivetenin: Sosyal aktivitenin geliştirir bireyin kendini iyi hisetmesini sağlar iletişimi gelişir",
+    "Obez: Sigara alkol kullanımı \n Eğitim Düzeyi \n yetersiz etkinlik \n Genetik Etmenler \n Gelir durumu",
+
+    "Zihin dugyusal: sosyoekonomik durum yangın salgın deprem  terör gibi olaylar zihinsel duygusal ve sosyal sağlıgı olumsuz etkiler",
+    "Birey stres: Mükemelletçiyelik \n sınavlar \nyüksek beklenti \nkıyaslama\n Aile sorunları",
+    "Madde: İç organların tümünün zarar görmesini ve buna bağlı hastalıklar neden olabilir \n kişide zamanla kaygı korku düşmanlık ve paronaya meydana gelir",
+    "Teknoloji: Teknolojik ürünlerin uzun süreli kullanımı bulanık görme göz yorgunlugu vb \n araba süreken kaza riski artması",
+    "İlk yardım Hasta: Yaşamsal tehlikenin ortadan kalkması  \n iyileşme kolaylaşmatırlması",
+
 ];
 
 
@@ -18,12 +30,12 @@ let messageIndex2 = 0;
 function displayMessage1() {
     pElement.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
-    setTimeout(displayMessage2, 5000); 
+    setTimeout(displayMessage2, 10000); 
 }
 function displayMessage2() {
     pElement.textContent = messages1[messageIndex2];
     messageIndex2 = (messageIndex2 + 1) % messages1.length;
-    setTimeout(displayMessage1, 1000); 
+    setTimeout(displayMessage1, 7000); 
 }
 
 
